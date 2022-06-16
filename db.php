fa9837451486222e77ac2311384f1e5a2eef6d70
vigorjs <80097275+vigorjs@users.noreply.github.com>
2022-06-17 04:12:05 +0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+$host_name = 'todolistcerdas-server.mysql.database.azure.com';
+$db_user = 'bvoxiibugi';
+$db_pass = 'W343PL0UF17T788T$';
+$db_name = 'cit_crud';
+
+$con = mysqli_connect($host_name,$db_user,$db_pass,$db_name);
