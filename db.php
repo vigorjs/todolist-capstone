24e44f2354befedb0eb14808088e0460bc7ff7f9
vigorjs <80097275+vigorjs@users.noreply.github.com>
2022-06-17 04:59:36 +0700

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 
 $host_name = 'todolistcerdas-server.mysql.database.azure.com';
 $db_user = 'iwtvgimcfg';
-$db_pass = '122O003I71NF654Y$';
+$db_pass = 'Batassuci123';
 $db_name = 'cit_crud';
 
 $con = mysqli_connect($host_name,$db_user,$db_pass,$db_name);
